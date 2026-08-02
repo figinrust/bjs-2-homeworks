783332be163bace036af85cab923924a4c1b707a
NightOwl <denis.kuzyurin@yandex.ru>
2026-08-02 19:26:42 +0300

    Homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.excluded) {
+        return;
+    }
+    
+    this.marks.push(...marksToAdd);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+
+    let result = this.marks.reduce((acc, curr) => acc + curr, 0);
+    
+    return result / this.marks.length;
+
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
