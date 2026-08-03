c0237952ff2845a236585ce85d57b32db976917d
NightOwl <denis.kuzyurin@yandex.ru>
2026-08-03 21:59:33 +0300

    Homework 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,64 @@
-﻿
+﻿function parseCount(number) {
+    let result = Number.parseFloat(number);
+  
+    if (Number.isNaN(result)) {
+        throw new Error('Невалидное значение');
+    }
+
+    return result;
+}
+
+function validateCount(number) {
+    try {
+        return parseCount(number);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        this.validate();
+    }
+
+    validate() {
+        if (
+            this.a + this.b <= this.c ||
+            this.a + this.c <= this.b ||
+            this.b + this.c <= this.a
+        ) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+
+        return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
